796ea533c3f8ac6e0277978890d4072634336812
Richard Chalk <barmybrit@gmail.com>
2025-04-04 07:17:55 +0200

    Finished

---
+++ SuperHeroAPIDemo_G_NET9/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPIDemo_G_NET9.Models;
+
+namespace SuperHeroAPIDemo_G_NET9.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+
+}
--- SuperHeroAPIDemo_G_NET9/Program.cs
+++ SuperHeroAPIDemo_G_NET9/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPIDemo_G_NET9.Data;
+
 namespace SuperHeroAPIDemo_G_NET9
 {
     public class Program
@@ -13,12 +16,28 @@ namespace SuperHeroAPIDemo_G_NET9
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // Lägg till min DbContext
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Lägg till min DataInitializer med Dependency Injection
+            builder.Services.AddTransient<DataInitializer>();
+
             var app = builder.Build();
+
+            // Kör min MigrateData() metod
+            using (var scope = app.Services.CreateScope())
+            {
+                scope.ServiceProvider.GetService<DataInitializer>().MigrateData();
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwaggerUI(options =>
+                    options.SwaggerEndpoint("/openapi/v1.json", "superHero api"));
+
             }
 
             app.UseHttpsRedirection();
@@ -27,6 +46,9 @@ namespace SuperHeroAPIDemo_G_NET9
 
 
             app.MapControllers();
+
+            app.MapGet("/", () => Results.Redirect("/swagger"))
+                .ExcludeFromDescription();
 
             app.Run();
         }
